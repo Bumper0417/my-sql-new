13bc2dffd4bf48787e91be7f24c75ff809f50b1d
Bumper0417 <chrisgoros13@gmail.com>
2019-12-19 09:20:45 +0000

    removed sql file

---
+++ mysql-from-python.py
@@ -0,0 +1,27 @@
+import os 
+import pymysql
+
+# Get username from gitpod environment
+# (Modify this variable if runnin on another environment)
+
+username = os.getenv('C9_USER')
+
+# Connect to the database
+connection = pymysql.connect(host = 'localhost',
+                            user = username,
+                            password = '',
+                            db = 'Chinook')
+
+try:
+    #Run a query
+    with connection.cursor() as cursor:
+        sql = "SELECT * from Artist;"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        print(result)
+
+finally:
+    # Close the connection, regardless of whether the above is correct
+    connection.close()
+
+
